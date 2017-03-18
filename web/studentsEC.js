22ab6282f211b43dfa82f401422cb9baa859dde4
Mark Ryan <markryandev@gmail.com>
2017-03-18 15:29:10 -0600

    Added Form to dialog, also downgraded Angular to 1.5.5

--- web/studentsEC.js
+++ web/studentsEC.js
@@ -6,7 +6,7 @@ function yearNumberToWord(year) {
     if (year > 4) return "You have attended College for longer than 4 years. Congrats, you must be a CS student :)  Good Luck";
     return "Error";
 }
-angular.module('app', ['ngMaterial'])
+angular.module('app', ['ngMaterial', 'ngMessages'])
     .factory('studentService', function($http) {
         //HTTP requests go here
         let studentService = {
@@ -17,19 +17,16 @@ angular.module('app', ['ngMaterial'])
         return studentService;
     })
     .controller('studentsController', ['$scope', 'studentService', '$http', '$mdDialog',
-        function studentsController($scope, studentService, $http, $mdDialog) {
-        $scope.students = [];
-        $scope.deletedStudents = [];
-        studentService.getStudents().then(function(res) {
-            _.each(res.data, function(id){
-                $http.get(`/api/v1/students/${id}.json`).then(function(res){
-                    let student = res.data;
-                    student.id = id;
+        function ($scope, studentService, $http, $mdDialog) {
+        //$scope variables
+        $scope.students = [];//array of students in table
+        $scope.deletedStudents = [];//deleted students (for restore capabilities)
+        $scope.addOrEditMessage = $scope.editing ? "Editing a Student" : "Adding a New Student";
+        $scope.editing = false;//toggle for whether we are editing or adding a student
+        $scope.studentToEdit;//reference to student being edited
+        $scope.studentToEditOld;//student being edited's old state (if changes are rejected)
 
-                    $scope.students.push(student);
-                });
-            });
-        });
+        //$scope functions
         $scope.removeStudent = function(student){
             $http.delete(`/api/v1/students/${student.id}.json`);
             let deletedStudent = _.remove($scope.students, student)[0];
@@ -43,13 +40,17 @@ angular.module('app', ['ngMaterial'])
                 $scope.students.push(studentToRestore);
             })
         }
-        $scope.showAdvanced = function(ev) {
+        $scope.addNew = function(ev) {
+            $scope.editing = false;
+            $scope.studentToEdit = {};
             $mdDialog.show({
-                controller: studentsController,
-                templateUrl: 'dialog1.tmpl.html',
+                controller: dialogController,
+                templateUrl: 'addOrEditDialog.tmpl.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
-                clickOutsideToClose:true,
+                clickOutsideToClose: false,
+                escapeToClose: false,
+                scope: $scope,
                 fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
             })
             .then(function(answer) {
@@ -58,5 +59,31 @@ angular.module('app', ['ngMaterial'])
                 $scope.status = 'You cancelled the dialog.';
             });
         };
+
+        //configuration/setup
+        studentService.getStudents().then(function(res) {
+            _.each(res.data, function(id){
+                $http.get(`/api/v1/students/${id}.json`).then(function(res){
+                    let student = res.data;
+                    student.id = id;
+
+                    $scope.students.push(student);
+                });
+            });
+        });
 }]);
+
+function dialogController($scope, $mdDialog){
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+}
  
